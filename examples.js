f1a7d04dcf3d7753109d9824ed2ba93b2c98bd14
Ievgen Bezuglyi <ievgen@revio.no>
2015-04-17 22:32:27 +0200

    added presentation

---
+++ examples.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ibezuglyi on 2015-03-25.
+ */
+
+http://jsfiddle.net/wf8Lsjy1/2/
+
+http://jsfiddle.net/c1m8ro1r/3/
+
+http://jsfiddle.net/qgu5vqx0/9/
+
+    http://jsfiddle.net/qjosfyc1/3/
+
